66c9461e04e50bf405d47080f6ce16e99ec2ec4c
lizb <loo4love90@gmail.com>
2026-03-31 18:51:42 +0900

    feat: post api

--- frontend/src/pages/posts/PostCreate.jsx
+++ frontend/src/pages/posts/PostCreate.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './PostCreateEdit.scss'
+import './PostPagesAll.scss'
 const PostCreate = () => {
   return (
-    <div>PostCreate</div>
+    <section className='page post-section post-create'>
+      <div className="inner">
+        <form action="">
+          <div className="post-card">
+            <div className="post-field">
+              <label className='post-label'>카테고리</label>
+              <div className="post-input-wrap">
+                <select >
+                  <option value=""></option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
   )
 }
 
