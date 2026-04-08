c15eeb993412531cbdb857c24eeec703480be280
lizb <loo4love90@gmail.com>
2026-04-08 18:32:01 +0900

    feat: tag api backend update

--- backend/src/main/java/picstory/backend/web/dto/PostResponse.java
+++ backend/src/main/java/picstory/backend/web/dto/PostResponse.java
@@ -4,6 +4,7 @@ import picstory.backend.domain.Post;
 import picstory.backend.domain.PostCategory;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record PostResponse(
         Long id,
@@ -11,18 +12,22 @@ public record PostResponse(
         String title,
         String content,
         String imageUrl,
+        List<String> tags,
         Long memberId,
         String memberName,
         LocalDateTime createdAt
 ) {
 
-    public static PostResponse from(Post post){
-        return  new PostResponse(
+    public static PostResponse from(Post post) {
+        return new PostResponse(
                 post.getId(),
                 post.getCategory(),
                 post.getTitle(),
                 post.getContent(),
                 post.getImageUrl(),
+                post.getTags().stream()
+                        .map(tag -> tag.getLabel())
+                        .toList(),
                 post.getMember().getId(),
                 post.getMember().getName(),
                 post.getCreatedAt()
